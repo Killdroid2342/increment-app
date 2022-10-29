39ee83bb83af3d222c58b85535a72ecd46a027dc
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-10-29 19:06:12 +0100

    "project final with tailwind"

--- src/components/Count/counter.js
+++ src/components/Count/counter.js
@@ -4,14 +4,10 @@ const Counter = () => {
   const [up, setUp] = useState(0);
 
   const add = () => {
-    // console.log("TEST");
-
     setUp(up + 1);
   };
 
   const away = () => {
-    // console.log("test2");
-
     setUp(up - 1);
   };
 
@@ -21,19 +17,17 @@ const Counter = () => {
 
   return (
     <div className='center'>
-      <div className='center w-52 h-48 border-2 border-white mt-80'>
+      <div className='center w-60 h-48 border-2 border-white mt-80'>
         <div className='border-2 border-white pl-4 pr-4'>{up}</div>
         <div>
           <button className='btn pl-3 pr-3 mr-4' onClick={add}>
             +
           </button>
-
           <button className=' btn pl-3.5 pr-3.5 mb-7' onClick={away}>
             -
           </button>
         </div>
-
-        <button onClick={reset} className=''>
+        <button onClick={reset} className='border-2 border-white pl-3 pr-3'>
           Click here to reset
         </button>
       </div>
