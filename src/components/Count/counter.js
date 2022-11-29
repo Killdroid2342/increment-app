0d24718ac1c6d36bf1f96c38ad44aa8e43c497ec
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-29 21:43:03 +0000

    added rounded-2xl

--- src/components/Count/counter.js
+++ src/components/Count/counter.js
@@ -32,40 +32,40 @@ const Counter = () => {
   return (
     <div className='flex flex-col items-center justify-center'>
       <h1 className='text-3xl mt-52 font-bold'>You are on {up}</h1>
-      <div className='flex flex-col items-center justify-center p-10 h-48 border-2 border-white mt-20 rounded-lg'>
+      <div className='flex flex-col items-center justify-center p-10 h-48 border-2 border-white mt-20 rounded-2xl'>
         <div>
           <button
-            className='border-2 border-white pt-1 pb-1 mt-3 pl-3 pr-3 mr-4 font-bold'
+            className='border-2 border-white pt-1 pb-1 mt-3 pl-3 pr-3 mr-4 font-bold rounded-2xl'
             onClick={addTen}
           >
             + 10
           </button>
           <button
-            className='border-2 border-white pt-1 pb-1 mt-3 pl-3 pr-3 mr-4 font-bold'
+            className='border-2 border-white pt-1 pb-1 mt-3 pl-3 pr-3 mr-4 font-bold rounded-2xl'
             onClick={addOne}
           >
             + 1
           </button>
           <button
-            className='border-2 border-white pt-1 pb-1 mt-3 pl-3.5 pr-3 mb-7 mr-4 font-bold'
+            className='border-2 border-white pt-1 pb-1 mt-3 pl-3.5 pr-3 mb-7 mr-4 font-bold rounded-2xl'
             onClick={addFive}
           >
             + 5
           </button>
           <button
-            className='border-2 border-white pt-1 pb-1 mt-3 pl-3.5 pr-3 mb-7 mr-4 font-bold'
+            className='border-2 border-white pt-1 pb-1 mt-3 pl-3.5 pr-3 mb-7 mr-4 font-bold rounded-2xl'
             onClick={awayOne}
           >
             - 1
           </button>
           <button
-            className='border-2 border-white pt-1 pb-1 mt-3 pl-3.5 pr-3 mb-7 mr-4 font-bold'
+            className='border-2 border-white pt-1 pb-1 mt-3 pl-3.5 pr-3 mb-7 mr-4 font-bold rounded-2xl'
             onClick={awayFive}
           >
             - 5
           </button>
           <button
-            className='border-2 border-white pt-1 pb-1 mt-3 pl-3.5 pr-3 mb-7 font-bold'
+            className='border-2 border-white pt-1 pb-1 mt-3 pl-3.5 pr-3 mb-7 font-bold rounded-2xl'
             onClick={awayTen}
           >
             - 10
